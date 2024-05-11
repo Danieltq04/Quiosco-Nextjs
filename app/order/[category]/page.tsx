4c263276616eb78bd6933ecc1efdaf14fbcdcc7e
Danieltq04 <daniel.tq04@gmail.com>
2024-05-11 12:47:00 -0300

    Iteración sobre productos

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import ProductCard from "@/components/products/ProductCard"
+import { prisma } from "@/src/lib/prisma"
 
-export default function OrderPage({params}:{ params: { category : string}}) {
-  console.log(params.category)
+async function getProducts(category:string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug : category
+      }
+    }
+  })
+  return products
+}
+
+export default async function OrderPage({params}:{ params: { category : string}}) {
+  const products = await getProducts(params.category)
+  console.log(products)
   return (
-    <div>
-      OrderPage
-    </div>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
+        {products.map(product => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </div>
+    </>
+
   )
 }
